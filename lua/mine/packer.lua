2f5f36a8d4be6682fff7308de35908bc589f7869
Christian Apolloni <christian.apolloni@gmail.com>
2023-12-10 11:55:30 +0100

    Add Vim-Commentary

--- lua/mine/packer.lua
+++ lua/mine/packer.lua
@@ -68,6 +68,9 @@ return require('packer').startup(function(use)
             {'hrsh7th/cmp-nvim-lsp'},
             {'L3MON4D3/LuaSnip'},
         }
-    } 
+    }
+
+    use('tpope/vim-commentary')
+
 end)
 
