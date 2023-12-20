13ea09c2b0bd9806c92248c7743fb1176dfbfba7
Christian Apolloni <christian.apolloni@gmail.com>
2023-12-20 19:59:22 +0100

    Focus to UndoTree when toggled

--- lua/mine/lazy.lua
+++ lua/mine/lazy.lua
@@ -83,6 +83,7 @@ require("lazy").setup({
     {
         'mbbill/undotree',
         config = function()
+            vim.g.undotree_SetFocusWhenToggle = true
             vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
         end
     },
