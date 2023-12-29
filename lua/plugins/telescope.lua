f522dc5d8ae4330bd6c181a399a0eb3ec08a43b6
Christian Apolloni <christian.apolloni@gmail.com>
2023-12-29 15:47:18 +0100

    Add git status in telescope

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -29,6 +29,7 @@ return {
             local builtin = require('telescope.builtin')
             vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = "Find Files" })
             vim.keymap.set('n', '<leader>fg', builtin.git_files, { desc = "Git Files" })
+            vim.keymap.set('n', '<leader>fs', builtin.git_status, { desc = "Git Status" })
             vim.keymap.set('n', '<leader>fr', builtin.live_grep, { desc = "Live Grep" })
         end
     },
