5065eae929d1bddd8ef8ab6ea69ddbbe14d87044
Christian Apolloni <christian.apolloni@gmail.com>
2024-01-07 18:54:45 +0100

    Reenable delete operation in nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -8,19 +8,10 @@ return {
         local function my_on_attach(bufnr)
             local api = require("nvim-tree.api")
 
-            local function opts(desc)
-                return { desc = "nvim-tree: " .. desc, buffer = bufnr, noremap = true, silent = true, nowait = true }
-            end
-
             api.config.mappings.default_on_attach(bufnr)
 
-            vim.keymap.del("n", "d", opts(""))
-            vim.keymap.del("n", "D", opts(""))
-            vim.keymap.del("n", "bd", opts(""))
-            vim.keymap.del("n", "bt", opts(""))
-
-            vim.keymap.set("n", "<S-Right>", "<cmd>NvimTreeResize +10<CR>", {desc = "Widen nvim-tree" })
-            vim.keymap.set("n", "<S-Left>", "<cmd>NvimTreeResize -10<CR>", {desc = "Narrow nvim-tree" })
+            vim.keymap.set("n", "<S-Right>", "<cmd>NvimTreeResize +10<CR>", { desc = "Widen nvim-tree" })
+            vim.keymap.set("n", "<S-Left>", "<cmd>NvimTreeResize -10<CR>", { desc = "Narrow nvim-tree" })
         end
 
         vim.g.loaded_netrw = 1
