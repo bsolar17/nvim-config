2cbd087cdf9eb2ed0ef0bd0b6ccd25124c83208f
Christian Apolloni <christian.apolloni@gmail.com>
2024-01-08 20:01:28 +0100

    Add git blame line config to gitsigns

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -8,5 +8,10 @@ return {
         },
         numhl = true,
         attach_to_untracked = false,
-    }
+    },
+    config = function(_, opts)
+        require("gitsigns").setup(opts)
+        vim.keymap.set("n", "<Leader>b", "<cmd>lua require('gitsigns').blame_line({ full = true})<CR>",
+            { desc = "Git Blame" })
+    end
 }
