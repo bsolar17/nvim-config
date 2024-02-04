e139c5ed22b857437e5e35e9b9f23e516bf26066
Christian Apolloni <christian.apolloni@gmail.com>
2024-02-04 15:06:42 +0100

    Change jdtls components location

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,7 +1,7 @@
 local bundles = {
-    vim.fn.glob("~/Projects/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar", 1),
+    vim.fn.glob("~/Projects/jdtls/artifacts/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar", 1),
 };
-vim.list_extend(bundles, vim.split(vim.fn.glob("~/Projects/vscode-java-test/server/*.jar", 1), "\n"))
+vim.list_extend(bundles, vim.split(vim.fn.glob("~/Projects/jdtls/artifacts/vscode-java-test/server/*.jar", 1), "\n"))
 local config = {
     cmd = { "jdtls" },
     root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
