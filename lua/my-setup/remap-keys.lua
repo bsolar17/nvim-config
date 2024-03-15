1c2bea8fc72d4f562eb9378050a556591c70106d
Christian Apolloni <christian.apolloni@gmail.com>
2024-03-15 21:45:29 +0100

    Add buffer navigation keymaps

--- lua/my-setup/remap-keys.lua
+++ lua/my-setup/remap-keys.lua
@@ -1,2 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
+
+vim.keymap.set("n", "<Leader>bp", "<cmd>bprevious<cr>", { desc = "Previous" })
+vim.keymap.set("n", "<Leader>bn", "<cmd>bnext<cr>", { desc = "Next" })
+vim.keymap.set("n", "<Leader>bd", "<cmd>bdelete<cr>", { desc = "Delete" })
