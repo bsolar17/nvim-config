bdabaef4ea0b701e298dda9f273d22eb5f2b76e2
Christian Apolloni <christian.apolloni@gmail.com>
2024-03-17 10:48:57 +0100

    Reimplement buffer jum with Lualine function

--- lua/my-setup/remap-keys.lua
+++ lua/my-setup/remap-keys.lua
@@ -1,19 +1,6 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
-local function select_buf(n)
-    local buffers = vim.api.nvim_command_output("buffers")
-    local buf_list = vim.split(buffers, "\n")
-    local buf_indexes_list = vim.tbl_map(function(buf) return tonumber(string.match(buf, "%d+")) end, buf_list)
-    local buf_index = buf_indexes_list[n]
-    if buf_index then
-        vim.cmd("buffer " .. buf_index)
-    end
-end
-for i = 1, 8 do
-    vim.keymap.set("n", "<Leader>b" .. i, function() select_buf(i) end, { desc = tostring(i) })
-end
-
 vim.keymap.set("n", "<Leader>bp", "<cmd>bprevious<cr>", { desc = "Previous" })
 vim.keymap.set("n", "<Leader>bn", "<cmd>bnext<cr>", { desc = "Next" })
 vim.keymap.set("n", "<Leader>bd", "<cmd>bdelete<cr>", { desc = "Delete" })
--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -11,4 +11,16 @@ return {
             lualine_c = { "buffers" },
         },
     },
+    config = function(_, opts)
+        require("lualine").setup(opts)
+        local function safeBuffersJump(n)
+            local ok, _ = pcall(vim.cmd, "LualineBuffersJump " .. n)
+            if not ok then
+                vim.print("Error jumping to buffer " .. n)
+            end
+        end
+        for i = 1, 8 do
+            vim.keymap.set("n", "<leader>b" .. i, function() safeBuffersJump(i) end, { desc = tostring(i) })
+        end
+    end
 }
