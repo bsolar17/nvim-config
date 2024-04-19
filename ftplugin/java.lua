b1a94fd99713d7f41d779de648f77197a1a7b272
Christian Apolloni <christian.apolloni@gmail.com>
2024-04-19 11:15:01 +0200

    Move java-debug artifacts

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -1,5 +1,5 @@
 local bundles = {
-    vim.fn.glob("~/Projects/jdtls/artifacts/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar", 1),
+    vim.fn.glob("~/Projects/jdtls/artifacts/java-debug/com.microsoft.java.debug.plugin/com.microsoft.java.debug.plugin-*.jar", 1),
 };
 vim.list_extend(bundles, vim.split(vim.fn.glob("~/Projects/jdtls/artifacts/vscode-java-test/server/*.jar", 1), "\n"))
 local config = {
