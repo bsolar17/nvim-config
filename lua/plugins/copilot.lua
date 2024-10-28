555d66d9823400153884a6926a263b91453578fb
Christian Apolloni <christian.apolloni@gmail.com>
2024-10-28 19:34:05 +0100

    Disable Copilot on startup

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -3,7 +3,15 @@ return {
         "zbirenbaum/copilot.lua",
         lazy = true,
         event = { "InsertEnter", "CmdlineEnter" },
-        opts = {},
+        opts = {
+            suggestion = { enabled = false },
+            panel = { enabled = false },
+        },
+        config = function(_, opts)
+            local copilot = require("copilot")
+            copilot:setup()
+            vim.cmd(":Copilot disable")
+        end,
     },
     {
         "zbirenbaum/copilot-cmp",
