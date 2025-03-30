088e005a2074bb6ba7f02e2dd7be45b2cb08e5bb
Christian Apolloni <christian.apolloni@gmail.com>
2025-03-30 18:17:33 +0200

    Refine blink.cmp configuration

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -13,8 +13,12 @@ return {
             nerd_font_variant = "mono",
         },
         completion = {
+            menu = {
+                auto_show = true,
+            },
             documentation = {
                 auto_show = true,
+                auto_show_delay_ms = 0,
             },
             list = {
                 selection = {
@@ -33,6 +37,22 @@ return {
         },
         fuzzy = {
             implementation = "prefer_rust_with_warning",
+        },
+        cmdline = {
+            keymap = {
+                ['<CR>'] = { 'accept', 'fallback' },
+            },
+            completion = {
+                menu = {
+                    auto_show = true,
+                },
+                list = {
+                    selection = {
+                        preselect = false,
+                        auto_insert = false,
+                    },
+                },
+            },
         },
     },
     opts_extend = { "sources.default" }
