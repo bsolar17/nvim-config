a6b2452f100b606f2a9bdf014f896b1df42a9ce8
Christian Apolloni <christian.apolloni@gmail.com>
2025-04-03 22:33:24 +0200

    Allow to toggle using prettier for Java on demand

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -9,5 +9,17 @@ return {
         local conform = require("conform")
         conform.setup(opts)
         vim.keymap.set({ "n", "x", "v" }, "<Leader>cf", conform.format, { desc = "Format" })
+        vim.keymap.set("n", "<Leader>cp", function()
+            if opts and opts.formatters_by_ft and vim.tbl_contains(opts.formatters_by_ft.java or {}, "prettier") then
+                opts.formatters_by_ft.java = nil
+                print("Prettier disabled for Java")
+            else
+                opts = opts or {}
+                opts.formatters_by_ft = opts.formatters_by_ft or {}
+                opts.formatters_by_ft.java = { "prettier" }
+                print("Prettier enabled for Java")
+            end
+            conform.setup(opts)
+        end, { desc = "Toggle Prettier for Java" })
     end,
 }
