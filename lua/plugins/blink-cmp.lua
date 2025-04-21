584fce0094c8ba79266ae87599484446e7392952
Christian Apolloni <christian.apolloni@gmail.com>
2025-04-21 21:56:50 +0200

    Auto show blink.cmp documentation

--- lua/plugins/blink-cmp.lua
+++ lua/plugins/blink-cmp.lua
@@ -30,6 +30,10 @@ return {
                     auto_insert = false,
                 },
             },
+            documentation = {
+                auto_show = true,
+                auto_show_delay_ms = 500,
+            },
         },
         sources = {
             default = {
