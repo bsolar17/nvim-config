bf5e6992c3ff65e67d451e24ccb35dd20af4a8be
Christian Apolloni <christian.apolloni@gmail.com>
2025-05-14 20:15:40 +0200

    Add lombok to jdtls configuration

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -2,12 +2,15 @@ if vim.fn.executable("jdtls") == 1 then
     local path_to_mason_share = os.getenv("XDG_DATA_HOME") .. "/nvim/mason/share"
     local path_to_java_debug = path_to_mason_share .. "/java-debug-adapter"
     local path_to_java_test = path_to_mason_share .. "/java-test"
+    local path_to_lombok = path_to_mason_share .. "/jdtls/lombok.jar"
     local bundles = {
         vim.fn.glob(path_to_java_debug .. "/com.microsoft.java.debug.plugin.jar", true),
     }
     vim.list_extend(bundles, vim.split(vim.fn.glob(path_to_java_test .. "/*.jar", true), "\n"))
     local config = {
-        cmd = { "jdtls" },
+        cmd = vim.fn.filereadable(path_to_lombok) == 1
+            and { "jdtls", "--jvm-arg=-javaagent:" .. path_to_lombok }
+            or { "jdtls" },
         root_dir = vim.fs.dirname(vim.fs.find({ "gradlew", ".git", "mvnw" }, { upward = true })[1]),
         settings = {
             java = {
