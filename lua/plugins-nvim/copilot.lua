6a2acd8ee5eaf134802be1a5cdf8d8def0e32ab1
Christian Apolloni <christian.apolloni@gmail.com>
2025-06-13 19:16:00 +0200

    Add CopilotChat explain keymaps

--- lua/plugins-nvim/copilot.lua
+++ lua/plugins-nvim/copilot.lua
@@ -21,7 +21,27 @@ return {
         },
         config = function(_, opts)
             require("CopilotChat").setup(opts)
-            vim.keymap.set("n", "<Leader>C", "<cmd>CopilotChatToggle<cr>", { desc = "Copilot" })
+            vim.keymap.set(
+                "n",
+                "<Leader>C",
+                "<cmd>CopilotChatToggle<cr>",
+                { desc = "Copilot" }
+            )
+            vim.keymap.set(
+                "v",
+                "<Leader>ce",
+                "<cmd>CopilotChatExplain<cr>",
+                { desc = "Explain" }
+            )
+            vim.keymap.set(
+                "n",
+                "<Leader>ce",
+                function()
+                    vim.cmd("normal! V")
+                    vim.cmd("CopilotChatExplain")
+                end,
+                { desc = "Explain" }
+            )
         end,
     },
 }
