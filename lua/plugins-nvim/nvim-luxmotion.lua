28a99e9bd3f7f28728e5c5c256400d97a88820f7
Christian Apolloni <christian.apolloni@gmail.com>
2025-09-26 20:54:40 +0200

    Add nvim-luxmotion

---
+++ lua/plugins-nvim/nvim-luxmotion.lua
@@ -0,0 +1,22 @@
+return {
+    "LuxVim/nvim-luxmotion",
+    opts = {
+        cursor = {
+            duration = 100,
+            easing = "linear",
+            enabled = true,
+        },
+        scroll = {
+            duration = 200,
+            easing = "linear",
+            enabled = true,
+        },
+        performance = {
+            enabled = true
+        },
+        keymaps = {
+            cursor = true,
+            scroll = true,
+        },
+    },
+}
