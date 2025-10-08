47cccb3860be701b66d42ae17961bd67184f288d
Christian Apolloni <christian.apolloni@gmail.com>
2025-10-08 22:17:03 +0200

    Remove treesitter ensure_installed config

--- lua/plugins-nvim/treesitter.lua
+++ lua/plugins-nvim/treesitter.lua
@@ -3,7 +3,6 @@ return {
         "nvim-treesitter/nvim-treesitter",
         build = ":TSUpdate",
         opts = {
-            ensure_installed = { "c", "lua", "vim", "vimdoc", "query" },
             sync_install = false,
             auto_install = true,
             highlight = {
