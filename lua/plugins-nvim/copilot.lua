efd056e993b1e525fb773ff8dfd870d9e36000c7
Christian Apolloni <christian.apolloni@gmail.com>
2025-10-21 12:34:21 +0200

    Add copilot.lua but only load it manually
    Only used to manage the Copilot token

--- lua/plugins-nvim/copilot.lua
+++ lua/plugins-nvim/copilot.lua
@@ -1,57 +1,71 @@
 return {
-    "olimorris/codecompanion.nvim",
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-    },
-    lazy = true,
-    keys = {
-        {
-            mode = "n",
-            "<Leader>cc",
-            "<cmd>CodeCompanionChat Toggle<cr>",
-            desc = "Chat",
-        },
-        {
-            mode = "v",
-            "<Leader>ce",
-            "<cmd>CodeCompanion /explain<cr>",
-            desc = "Explain",
-        },
-        {
-            mode = "n",
-            "<Leader>ce",
-            function()
-                vim.cmd("normal! V")
-                vim.cmd("CodeCompanion /explain")
-            end,
-            desc = "Explain",
+    {
+        "zbirenbaum/copilot.lua",
+        lazy = true,
+        opts = {
+            suggestion = {
+                enabled = false
+            },
+            panel = {
+                enabled = false
+            },
         },
     },
-    opts = {
-        strategies = {
-            chat = {
-                adapter = "copilot",
-                model = "claude-sonnet-4.5",
-                tools = {
-                    opts = {
-                        default_tools = {
-                            "full_stack_dev",
+    {
+        "olimorris/codecompanion.nvim",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+        },
+        lazy = true,
+        keys = {
+            {
+                mode = "n",
+                "<Leader>cc",
+                "<cmd>CodeCompanionChat Toggle<cr>",
+                desc = "Chat",
+            },
+            {
+                mode = "v",
+                "<Leader>ce",
+                "<cmd>CodeCompanion /explain<cr>",
+                desc = "Explain",
+            },
+            {
+                mode = "n",
+                "<Leader>ce",
+                function()
+                    vim.cmd("normal! V")
+                    vim.cmd("CodeCompanion /explain")
+                end,
+                desc = "Explain",
+            },
+        },
+        opts = {
+            strategies = {
+                chat = {
+                    adapter = "copilot",
+                    model = "claude-sonnet-4.5",
+                    tools = {
+                        opts = {
+                            default_tools = {
+                                "full_stack_dev",
+                            },
                         },
                     },
                 },
+                inline = {
+                    adapter = "copilot",
+                    model = "claude-sonnet-4.5",
+                },
+                cmd = {
+                    adapter = "copilot",
+                    model = "claude-sonnet-4.5",
+                },
             },
-            inline = {
-                adapter = "copilot",
-                model = "claude-sonnet-4.5",
-            },
-            cmd = {
-                adapter = "copilot",
-                model = "claude-sonnet-4.5",
-            },
-        },
-        display = {
-            chat = {
-                intro_message = "",
+            display = {
+                chat = {
+                    intro_message = "",
+                },
             },
         },
     },
