826bdad11fc7586619e494fc4b06d84acd4c404e
Christian Apolloni <christian.apolloni@gmail.com>
2025-12-04 21:29:04 +0100

    Add keymap to toggle a new tab split

--- lua/my-setup/remap-keys.lua
+++ lua/my-setup/remap-keys.lua
@@ -4,3 +4,10 @@ vim.g.maplocalleader = " "
 vim.keymap.set("n", "<Leader>bp", "<Cmd>bprevious<CR>", { desc = "Previous" })
 vim.keymap.set("n", "<Leader>bn", "<Cmd>bnext<CR>", { desc = "Next" })
 vim.keymap.set("n", "<Leader>bd", "<Cmd>bdelete<CR>", { desc = "Delete" })
+vim.keymap.set("n", "<Leader>bt", function()
+    if vim.fn.tabpagenr("$") == 1 then
+        vim.cmd("tab split")
+    else
+        vim.cmd("tabclose")
+    end
+end, { desc = "Tab Split/Close Toggle" })
