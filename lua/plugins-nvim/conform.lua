641ac6875dafc9f27cface0a3fa64f7be8be3996
Christian Apolloni <christian.apolloni@gmail.com>
2025-12-30 17:04:09 +0100

    Remove prettier from conform

--- lua/plugins-nvim/conform.lua
+++ lua/plugins-nvim/conform.lua
@@ -25,24 +25,5 @@ return {
                 end
             end
         end, { desc = "Format" })
-        vim.keymap.set("n", "<Leader>cp", function()
-            if
-                opts
-                and opts.formatters_by_ft
-                and vim.tbl_contains(
-                    opts.formatters_by_ft.java or {},
-                    "prettier"
-                )
-            then
-                opts.formatters_by_ft.java = nil
-                print("Prettier disabled for Java")
-            else
-                opts = opts or {}
-                opts.formatters_by_ft = opts.formatters_by_ft or {}
-                opts.formatters_by_ft.java = { "prettier" }
-                print("Prettier enabled for Java")
-            end
-            conform.setup(opts)
-        end, { desc = "Toggle Prettier for Java" })
     end,
 }
