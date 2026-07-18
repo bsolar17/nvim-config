9d096542fe153e3888ff83bfc8981cf0846693ef
Christian Apolloni <christian.apolloni@gmail.com>
2026-07-18 20:28:11 +0200

    Add reference copy keymaps

--- lua/my-setup/remap-keys.lua
+++ lua/my-setup/remap-keys.lua
@@ -25,3 +25,72 @@ vim.keymap.set({ "n", "x", "o" }, "<A-i>", function()
         vim.lsp.buf.selection_range(-vim.v.count1)
     end
 end, { desc = "Select In" })
+
+local function copy_and_notify(text)
+    vim.fn.setreg("+", text)
+    vim.notify(("Copied: " .. text))
+    return text
+end
+local function relative_filepath()
+    return "./" .. vim.fn.expand("%:.")
+end
+local function absolute_filepath()
+    return vim.fn.expand("%:p")
+end
+local function reference_line(filepath)
+    local line_number = vim.fn.line(".")
+    return copy_and_notify(filepath .. ":" .. line_number)
+end
+local function reference_word(filepath)
+    local word = vim.fn.expand("<cword>")
+    local line_number = vim.fn.line(".")
+    local word_with_filepath = (
+        "`"
+        .. word
+        .. "` ("
+        .. filepath
+        .. ":"
+        .. line_number
+        .. ")"
+    )
+    return copy_and_notify(word_with_filepath)
+end
+local function reference_line_range(filepath)
+    local start_line = vim.fn.getpos("v")[2]
+    local end_line = vim.fn.line(".")
+    local sorted_start = math.min(start_line, end_line)
+    local sorted_end = math.max(start_line, end_line)
+    local filepath_with_range = (
+        filepath
+        .. ":"
+        .. sorted_start
+        .. "-"
+        .. sorted_end
+    )
+    return copy_and_notify(filepath_with_range)
+end
+
+vim.keymap.set("n", "<Leader>rl", function()
+    return reference_line(relative_filepath())
+end, { desc = "Reference Line" })
+vim.keymap.set("n", "<Leader>ral", function()
+    return reference_line(absolute_filepath())
+end, { desc = "Reference Line (Absolute)" })
+vim.keymap.set("n", "<Leader>rf", function()
+    return copy_and_notify(relative_filepath())
+end, { desc = "Reference File" })
+vim.keymap.set("n", "<Leader>raf", function()
+    return copy_and_notify(absolute_filepath())
+end, { desc = "Reference File (Absolute)" })
+vim.keymap.set("n", "<Leader>rw", function()
+    return reference_word(relative_filepath())
+end, { desc = "Reference Word" })
+vim.keymap.set("n", "<Leader>raw", function()
+    return reference_word(absolute_filepath())
+end, { desc = "Reference Word (Absolute)" })
+vim.keymap.set("x", "<Leader>rv", function()
+    return reference_line_range(relative_filepath())
+end, { desc = "Reference Line Range" })
+vim.keymap.set("x", "<Leader>rav", function()
+    return reference_line_range(absolute_filepath())
+end, { desc = "Reference Line Range (Absolute)" })
