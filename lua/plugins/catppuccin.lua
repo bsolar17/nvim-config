c4bb1b8f544e28dd72f9c6ce73a60deec6b0ec7f
Christian Apolloni <christian.apolloni@gmail.com>
2026-09-04 20:13:13 +0200

    Upgrade catppuccin config

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -4,10 +4,11 @@ return {
     priority = 1000,
     name = "catppuccin",
     opts = {
+        flavour = "mocha",
         transparent_background = true,
     },
     config = function(_, opts)
         require("catppuccin").setup(opts)
-        vim.cmd("colorscheme catppuccin-mocha")
+        vim.cmd("colorscheme catppuccin-nvim")
     end,
 }
